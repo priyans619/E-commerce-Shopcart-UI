ea26aaa909b8edde17b620c421b59258aa297027
priyans619 <priyansh.2b@gmail.com>
2024-08-15 23:31:09 +0530

    add name, initialState, reducers for cartSllice

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,8 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
-
+  name: 'cart',
+  initialState: [],
+  reducers: {
+    addToCart: (state, action) => {
+      state.push(action.payload);
+    },
+    removeFromCart: (state, action) => {
+      return state.filter(item => item.id !== action.payload.id);
+    },
+  },
 });
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
 export const {} = cartSlice.actions;
 export default cartSlice.reducer;
