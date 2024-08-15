bde66d782010c42afdbe4fba9a441b860bead872
priyans619 <priyansh.2b@gmail.com>
2024-08-15 21:50:41 +0530

    add product and cart in reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+import productReducer from './productSlice';
+import cartReducer from './cartSlice';
 
 const rootReducer = combineReducers({
+  products: productReducer,
+  cart: cartReducer,
 
 });
 
