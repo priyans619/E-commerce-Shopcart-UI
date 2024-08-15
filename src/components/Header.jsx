b549ed157b5194665a3f6867cf500ec8e28fdf7d
priyans619 <priyansh.2b@gmail,com>
2024-08-15 20:10:24 +0530

    nav bar with names

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,12 @@ const Header = () => {
         <Link to="/">
           <img src={logo} alt="E-commerce" className="h-7 sm:h-8 md:h-10 lg:h-12" />
         </Link>
+        <nav>
+          <Link to="/products" className="text-white mx-3">Home</Link>
+          <Link to="/products" className="text-white mx-3">Products</Link>
+          <Link to="/cart" className="text-white mx-3">Cart</Link>
+          <Link to="/account" className="text-white mx-3">Account</Link>
+        </nav>
       </div>
     </header>
   )
