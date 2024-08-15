bda6aaf46d9589a15b2efd98e3e62718f0900b36
priyans619 <priyansh.2b@gmail,com>
2024-08-15 20:56:21 +0530

    route check

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <header className="fixed ">
-      <div className="container ">
+    <header className="bg-gray-900 p-4">
+      <div className="container mx-auto flex justify-between items-center">
         <Link to="/">
           <img src={logo} alt="E-commerce" className="h-7 sm:h-8 md:h-10 lg:h-12" />
         </Link>
         <nav>
-          <Link to="/products" className="text-white mx-3">Home</Link>
-          <Link to="/products" className="text-white mx-3">Products</Link>
-          <Link to="/cart" className="text-white mx-3">Cart</Link>
-          <Link to="/account" className="text-white mx-3">Account</Link>
+          <Link to="/" className="text-white mx-3">Home</Link>
+          <Link to="/" className="text-white mx-3">Products</Link>
+          <Link to="/" className="text-white mx-3">Cart</Link>
+          <Link to="/" className="text-white mx-3">Account</Link>
         </nav>
       </div>
     </header>
