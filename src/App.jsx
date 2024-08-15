da2d03d583e6136f80997044ab62cfdc942ec969
priyans619 <priyansh.2b@gmail.com>
2024-08-15 23:23:09 +0530

    Update App.jsx, wrapped in Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import Products from './pages/Products';
+
 
 const App = () => {
   return (
@@ -8,8 +11,13 @@ const App = () => {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow container mx-auto px-4">
-
+          <Routes>
+            <Route exact path="/" element={<Products />} />
+            <Route path="/products" element={<Products />} />
+            
+          </Routes>
         </main>
+        <Footer />
       </div>
     </Router>
   );
