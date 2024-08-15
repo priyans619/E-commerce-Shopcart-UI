cb761f528f255a5ee6cdfcb3bbe9866adee02e90
priyans619 <priyansh.2b@gmail,com>
2024-08-15 18:39:04 +0530

    Header class with logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="fixed ">
+      <div className="container ">
+        <Link to="/">
+          <img src={logo} alt="E-commerce" className="h-7 sm:h-8 md:h-10 lg:h-12" />
+        </Link>
+      </div>
+    </header>
   )
 }
 
