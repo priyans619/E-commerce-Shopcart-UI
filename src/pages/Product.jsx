c3a9b41463ba55ca2f032d223e2f2b4257e96d09
priyans619 <priyansh.2b@gmail.com>
2024-08-15 23:12:45 +0530

    displaying list of produts using dummy.json

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import ProductCard from '../components/ProductCard';
+import { setProducts } from '../redux/productSlice';
 
-const Product = () => {
+const Products = () => {
+  const dispatch = useDispatch();
+  const products = useSelector(state => state.products);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(data => dispatch(setProducts(data.products)));
+  }, [dispatch]);
+
   return (
-    <div>Product</div>
-  )
-}
+    <div className="container mx-auto mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4" >
+      {products.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
 
-export default Product
+export default Products;
