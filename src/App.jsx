f05ae47ae742ecbb6c0f1108f5a6acdef5684ba1
priyans619 <priyansh.2b@gmail,com>
2024-08-15 20:07:36 +0530

    render header component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
 
-
-function App() {
- 
-
+const App = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow container mx-auto px-4">
+
+        </main>
+      </div>
+    </Router>
+  );
+};
 
 export default App
