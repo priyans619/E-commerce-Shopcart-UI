43ddf941a54439c7b359999e8278ab3b2bee7717
priyans619 <priyansh.2b@gmail.com>
2024-08-16 18:21:58 +0530

    dollar conversion to rupees

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,7 +6,8 @@ const Cart = () => {
   const cart = useSelector(state => state.cart);
   const dispatch = useDispatch();
 
-  const cartItemCount = cart.length;
+  const cartItemCount = cart.length; //CartCount
+  const conversionRate = 82; //conversion rate from USD to INR
 
   return (
     <div className="container mx-auto mt-24">
@@ -17,10 +18,10 @@ const Cart = () => {
         <p>Your cart is empty.</p>
       ) : (
         cart.map(item => (
-          <div key={item.id} className="border p-4 mb-4 rounded-lg flex items-center shadow-md ">
+          <div key={item.id} className="border p-4 mb-4 rounded-lg flex items-center shadow-md">
             <div className="flex-grow">
               <h3 className="text-xl font-bold">{item.title}</h3>
-              <p>${item.price}</p>
+              <p>₹{(item.price * conversionRate).toFixed(2)}</p>
               <button
                 onClick={() => dispatch(removeFromCart(item))}
                 className="bg-red-400 text-white py-1 px-2 rounded mt-2"
