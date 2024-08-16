b9c1e7ddc4ba0f794a807bd37f09c99f7001ebc7
priyans619 <priyansh.2b@gmail.com>
2024-08-16 14:33:52 +0530

    Header links to respective routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <header className="bg-white p-4 border-b border-gray-400">
+    <header className="fixed top-0 left-0 right-0 bg-white p-4 border-b border-gray-400 z-10">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/">
           <img src={logo} alt="E-commerce" className="h-7 sm:h-8 md:h-10 lg:h-12" />
         </Link>
         <nav>
           <Link to="/" className="text-black text-xl mx-3">Home</Link>
-          <Link to="/" className="text-black text-xl mx-3">Products</Link>
-          <Link to="/" className="text-black text-xl mx-3">Cart</Link>
+          <Link to="/products" className="text-black text-xl mx-3">Products</Link>
+          <Link to="/cart" className="text-black text-xl mx-3">Cart</Link>
           <Link to="/" className="text-black text-xl mx-3">Account</Link>
         </nav>
       </div>
