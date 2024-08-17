9d02f139222b0e892124feaca560311e7ecdcd2f
priyans619 <priyansh.2b@gmail.com>
2024-08-17 13:00:22 +0530

    add quantity selector function for cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeFromCart } from '../redux/cartSlice';
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
+import { useState } from 'react';
 
 const Cart = () => {
   const cart = useSelector(state => state.cart);
   const dispatch = useDispatch();
+
+  // for managing quantities of items in the cart
+  const [quantities, setQuantities] = useState(cart.map(() => 1));
+
+  // Handle quantity change
+  const handleQuantityChange = (index, delta) => {
+    const newQuantities = [...quantities];
+    newQuantities[index] = Math.max(1, newQuantities[index] + delta);
+    setQuantities(newQuantities);
+  };
 
   const cartItemCount = cart.length; //CartCount
   const conversionRate = 82; //conversion USD to INR
@@ -17,11 +29,11 @@ const Cart = () => {
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        cart.map(item => (
+        cart.map((item, index) => (
           <div key={item.id} className="border p-4 mb-4 rounded-lg flex items-center shadow-md">
-            <div className="flex-grow">
+            <div className="flex-grow ">
               <h3 className="text-xl font-bold">{item.title}</h3>
-              <p>₹{(item.price * conversionRate).toFixed(2)}</p>
+              <p>₹{(item.price * conversionRate * quantities[index]).toFixed(2)}</p>
               <button
                 onClick={() => dispatch(removeFromCart(item))}
                 className="bg-red-400 text-white py-1 px-2 rounded mt-2"
@@ -29,7 +41,24 @@ const Cart = () => {
                 Remove item
               </button>
             </div>
-            <img src={item.thumbnail} alt={item.title} className="bg-gray-200 w-32 h-32 object-cover rounded ml-4" />
+            <div className="flex flex-col items-center ml-4">
+              <img src={item.thumbnail} alt={item.title} className="bg-gray-200 w-24 h-24 object-cover rounded" />
+              <div className="flex items-center mt-4">
+                <button
+                  onClick={() => handleQuantityChange(index, -1)}
+                  className="bg-gray-300 rounded-full p-1"
+                >
+                  <AiOutlineMinus />
+                </button>
+                <span className="mx-2 border border-gray-300 px-5 rounded">{quantities[index]}</span>
+                <button
+                  onClick={() => handleQuantityChange(index, 1)}
+                  className="bg-gray-300 rounded-full p-1"
+                >
+                  <AiOutlinePlus />
+                </button>
+              </div>
+            </div>
           </div>
         ))
       )}
