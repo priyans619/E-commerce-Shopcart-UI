7f32b6c1387584a4d4edef28114641db6dd2ef2c
priyans619 <priyansh.2b@gmail.com>
2024-08-17 22:18:03 +0530

    key-value seperation

--- src/components/CartDetails.jsx
+++ src/components/CartDetails.jsx
@@ -7,16 +7,28 @@ const CartDetails = ({ totalItems, totalPrice }) => {
 
   return (
     <div className="border p-4 mb-4 shadow-xl lg:mx-64 md:mx-24 sm:mx-24 bg-white">
-      <h2 className="text-xl text-gray-500 font-semibold">Price Details</h2>
+      <h2 className="text-xl text-gray-500 text-center font-semibold">Shopping Details</h2>
       <hr className="my-4" />
       <div className="mt-4 space-y-2">
-        <p>Price ({totalItems} items): ₹{totalPrice.toFixed(2)}</p>
-        <p>Discount: -₹{discount.toFixed(2)}</p>
-        <p>Delivery Charges: Free</p>
+        <div className="flex justify-between">
+          <p>Price ({totalItems} items):</p>
+          <p>₹{totalPrice.toFixed(2)}</p>
+        </div>
+        <div className="flex justify-between">
+          <p>Discount:</p>
+          <p className='text-green-600'>-₹{discount.toFixed(2)}</p>
+        </div>
+        <div className="flex justify-between">
+          <p>Delivery Charges:</p>
+          <p className='text-green-600'>Free</p>
+        </div>
         <hr className="my-4" />
-        <p className="text-lg font-medium py-1">Total Amount: ₹{finalAmount.toFixed(2)}</p>
+        <div className="flex justify-between text-lg font-medium py-1">
+          <p>Total Amount:</p>
+          <p>₹{finalAmount.toFixed(2)}</p>
+        </div>
         <hr className="my-4" />
-        <p className='font-medium text-green-600'>You will save 1000 ₹ on this order</p>
+        <p className='font-medium text-center text-green-600'>You will save ₹1000 on this order</p>
       </div>
     </div>
   );
