5120608b4d1284b6a4eaa5b2cfbf87ab45bd6693
priyans619 <priyansh.2b@gmail.com>
2024-08-17 21:25:41 +0530

    final footer modify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-gray-700 p-4">
+      <div className="container mx-auto text-center text-white">
+        &copy; 2024 E-Commerce. All rights reserved.
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
