a4cf4d069e464ec030d53003abac200f7175574b
priyans619 <priyansh.2b@gmail.com>
2024-08-18 16:18:38 +0530

    product card done

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -13,14 +13,14 @@ const ProductCard = ({ product }) => {
   };
 
   return (
-    <div className="rounded-lg p-1 border shadow-md mb-4 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto flex flex-col ">
+    <div className="rounded-sm p-5 bg-white border shadow-md mb-4 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto flex flex-col ">
       <div className="flex-grow">
-        <img src={product.thumbnail} alt={product.title} className="w-full h-56 object-cover mb-4 rounded-lg bg-gray-100" />
+        <img src={product.thumbnail} alt={product.title} className="w-full h-56 object-cover mb-4 rounded-md bg-slate-100" />
         <h2 className="text-xl h-8 overflow-hidden text-ellipsis whitespace-nowrap">{product.title}</h2>
         <p className="text-gray-700 font-bold">₹{(product.price * conversionRate).toFixed(2)}</p> {/* Price in INR */}
       </div>
       <button onClick={handleAddToCart}
-        className="bg-gray-200 text-gray-500 text-xl p-2 rounded mt-4"
+        className="bg-slate-100 hover:bg-blue-300 text-gray-500 text-xl p-1 rounded mt-4"
         >
         Add to Cart
       </button>
